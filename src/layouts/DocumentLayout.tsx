dd160b8af7a2e1e7c435a083233044159240400e
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-09-24 12:02:33 +0800

    Update DocumentLayout.tsx

--- src/layouts/DocumentLayout.tsx
+++ src/layouts/DocumentLayout.tsx
@@ -35,7 +35,7 @@ const DocumentLayout: FunctionComponent<LayoutProps> = ({
       />
 <div className="relative flex w-full">
   <div className="hidden lg:block absolute top-0 bottom-0 right-0 left-1/2 bg-white"></div>
-  <div className="relative flex w-full max-w-container mx-auto px-4 sm:px-6 lg:px-8">
+  <div className="relative flex w-full max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
     <div className="w-full flex-none lg:grid lg:grid-cols-3 lg:gap-8">
       <div className="bg-gray-50 lg:bg-transparent -mx-4 sm:-mx-6 lg:mx-0 py-12 sm:py-16 px-4 sm:px-6 lg:pl-0 lg:pr-8">
         <nav aria-label="Table of contents" className="text-sm max-w-[37.5rem] mx-auto lg:max-w-none lg:mx-0 relative lg:sticky lg:top-10" x-data="TableOfContents()" x-init="init()">
