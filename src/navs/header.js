81f529bbf0034b6107ffc9bad0eb9fcba896ce8c
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-09-24 09:34:55 +0800

    补充导航

--- src/navs/header.js
+++ src/navs/header.js
@@ -23,12 +23,15 @@ export const headerNav = {
       sections: [
         {
           id: 'platform',
-          name: '华炎魔方平台',
+          name: '平台',
           items: [
-            { name: '概览', href: '/platform/overview' },
+            { name: '低代码简介', href: '/platform/lowcode' },
+            { name: '华炎魔方平台', href: '/platform/overview' },
+            { name: '开源、开放', href: '/platform/open-source-low-code' },
             { name: '解决方案', href: '/platform/solutions' },
             { name: '功能', href: '/platform/features' },
             { name: '报价', href: '/platform/pricing' },
+            
           ],
         },
         {
