f448423364e005c4b35bcb735c540b26f2858758
baozhoutao <baozhoutao@hotoa.com>
2021-09-26 13:44:40 +0800

    支付完成界面，点击返回首页时，也进入订单详细页面
    https://github.com/steedos/steedos-apps/issues/123

--- src/lib/order.js
+++ src/lib/order.js
@@ -13,10 +13,9 @@ export async function submitOrder(orderInfo, router){
     try {
         const data =  await fetchAPI('/api/shop/order/submitOrder', {method: 'POST', body: JSON.stringify(orderInfo)})
         if(data.status === 'unpaid'){
-            window.location.href = `${ROOT_URL}/api/pay/weixin/${data.payment_id}?redirect_uri=${`${window.location.origin}/store/payment-success`}`
+            window.location.href = `${ROOT_URL}/api/pay/weixin/${data.payment_id}?redirect_uri=${`${window.location.origin}/store/payment-success?order=${data._id}`}`
         }else{
-            router.push(`/store/payment-success`)
-            
+            router.push(`/store/payment-success?order=${data._id}`);
         }
     } catch (Exception) {
         if(Exception.message === '401'){
--- src/pages/store/payment-success.js
+++ src/pages/store/payment-success.js
@@ -1,91 +1,80 @@
 import React from 'react'
-import Link from 'next/link'
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 import { CheckIcon } from '@heroicons/react/outline'
+import { ROOT_URL } from '@/lib/base.client'
+import { useRouter } from 'next/router'
+import {has, isArray} from 'lodash';
 
-export default class PaymentSuccess extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      home_url: '',
-      payment: {},
-      open: true
-    }
-  }
-  componentDidMount() {
-    if (window?.location?.search) {
-      const search = new URLSearchParams(window.location.search);
-      if (search.get("result")) {
-        this.setState({
-          payment: JSON.parse(decodeURIComponent(escape(window.atob(decodeURIComponent(search.get("result")))))),
-          home_url: window.location.origin,
-          open: true
-        })
+function openOrderInfo(orderId) {
+  window.open(`${ROOT_URL}/app/-/shop_orders/view/${orderId}`)
+}
+export default function PaymentSuccess() {
+  const router = useRouter()
+  const click = (params) => {
+    if(typeof window !== 'undefined' && router.query){
+      if(has(router.query, 'order')){
+        let orderId = router.query['order'];
+        if(isArray(orderId)){
+          orderId = orderId[orderId.length-1]
+        }
+        openOrderInfo(orderId);
       }
-    } else {
-      this.setState({
-        home_url: window.location.origin,
-        open: true
-      })
     }
-
+    router.replace('/')
   }
+  return (
+    <>
+      <Transition.Root show={true} as={Fragment}>
+        <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={() => { }}>
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"   
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
 
-  render() {
-    return (
-      <>
-        <Transition.Root show={this.state.open} as={Fragment}>
-          <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={() => { }}>
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-              </Transition.Child>
-
-              {/* This element is to trick the browser into centering the modal contents. */}
-              <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                &#8203;
-              </span>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-                  <div>
-                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                      <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                    </div>
-                    <div className="mt-3 text-center sm:mt-5">
-                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                        付款成功
-                      </Dialog.Title>
-                    </div>
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div>
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                    <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
                   </div>
-                  <div className="mt-5 sm:mt-6">
-                    <a href="/" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
-                      回到首页
-                    </a>
-
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                      付款成功
+                    </Dialog.Title>
                   </div>
                 </div>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
-      </>
-    )
-  }
+                <div className="mt-5 sm:mt-6">
+                  <a href="javascript:void (0)" onClick={click} className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm">
+                    回到首页
+                  </a>
+
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  )
 }
