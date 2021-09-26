f6bec582708497b7a5a7f0b25f9975f1a484c590
baozhoutao <baozhoutao@hotoa.com>
2021-09-26 13:31:19 +0800

    fix bug: 登录后，直接注销 异常 #14

--- src/lib/auth.client.js
+++ src/lib/auth.client.js
@@ -1,21 +1,31 @@
 import { fetchAPI, ROOT_URL } from '@/lib/base.client';
 
+function getRedirectUrl(href = window.location.href){
+	const redirect = href.replace("/steedos/sign-in", "").replace("/accounts/a/#/logout", "");
+	const u = new URL(redirect);
+	u.searchParams.delete('no_redirect');
+	u.searchParams.delete('X-Space-Id');
+	u.searchParams.delete('X-Auth-Token');
+	u.searchParams.delete('X-User-Id');
+	return u.toString();
+}
+
 export async function authValidate(){
     const data = await fetchAPI(`/api/v4/users/validate`, {method: 'POST', body: JSON.stringify({})})
     return data;
 }
 
 export function goLogin(){
-    window.location.href = `${ROOT_URL}/accounts/a/#/login?redirect_uri=${window.location.href}`
+    window.location.href = `${ROOT_URL}/accounts/a/#/login?redirect_uri=${getRedirectUrl()}`
 }
 
 export function goSignup(){
-    window.location.href = `${ROOT_URL}/accounts/a/#/signup?redirect_uri=${window.location.href}`
+    window.location.href = `${ROOT_URL}/accounts/a/#/signup?redirect_uri=${getRedirectUrl()}`
 }
 
 export function goLogout(){
     removeAuthInfo()
-    window.location.href = `${ROOT_URL}/accounts/a/#/logout?redirect_uri=${window.location.href}`
+    window.location.href = `${ROOT_URL}/accounts/a/#/logout?redirect_uri=${getRedirectUrl()}`
 }
 
 export function saveAuthInfo(userId, spaceId, authToken){
