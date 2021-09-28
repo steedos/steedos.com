ad1770c7d7b15d815bac89f889d6c61e561beca1
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-09-28 17:53:44 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -361,7 +361,7 @@ export function Header({site}) {
         </Dialog>
       </Transition.Root>
 
-      <header className="sticky z-10 lg:z-50 relative bg-white inset-0 flex-none text-sm font-medium ring-1 ring-gray-900 ring-opacity-5 shadow-sm">
+      <header className="sticky z-20 lg:z-50 relative bg-white inset-0 flex-none text-sm font-medium ring-1 ring-gray-900 ring-opacity-5 shadow-sm">
        
         <nav aria-label="Top" className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="">
