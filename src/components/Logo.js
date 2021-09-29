495ecda697f7a3016dd5eccc776e83ffb4c250a2
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-09-29 13:45:46 +0800

    logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,8 +6,13 @@ const defaultLogSrc = require("@/img/logo_platform.png")
 export function Logo(props) {
   let logoSrc =  props.src || defaultLogSrc
   return (
-    <Image
-      src={logoSrc}
+    // <Image
+    //   src={logoSrc}
+    //   className={props.className}
+    //   height={40}
+    //   width={128}
+    // />
+    <img src="/logo.png" 
       className={props.className}
       height={40}
       width={128}
