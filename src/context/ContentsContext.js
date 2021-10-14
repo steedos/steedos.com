76e7b62dd4300c18ef261e16157ab0517f862bb5
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-10-14 10:08:35 +0800

    Delete ContentsContext.js

--- src/context/ContentsContext.js
+++
@@ -1,9 +0,0 @@
-export const ContentsContext = React.createContext({})
-
-export const ContentsContext = ({children}) => {
-  return (
-    <ContentsContext.Provider value={{...values}}>
-      {children}
-    </ContentsContext.Provider>
-  )
-}
