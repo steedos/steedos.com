43c4f4e2eeaf8a219e4754e6e1f34d9a5fca23ed
baozhoutao <baozhoutao@hotoa.com>
2021-10-20 17:24:51 +0800

    fix bug: 购物车记录删除后弹框不消失 #50

--- src/components/Trash.js
+++ src/components/Trash.js
@@ -71,7 +71,10 @@ export default function Trash({title, onRemove}) {
                         <button
                     type="button"
                     className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                    onClick={onRemove}
+                    onClick={async ()=>{
+                      await onRemove();
+                      closeModal();
+                    }}
                   >
                     继续
                   </button>
