3c2044a8eb493965b87a47d98b1ae29eced25f84
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-10-30 17:44:53 +0800

    tailwind jit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -23,14 +23,13 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6" />
-          <link rel="stylesheet" media="all" href="/css/tailwind-min.css" />
+          {/* <link rel="stylesheet" media="all" href="/css/tailwind-min.css" /> */}
   
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script> </script>
         </body>
         <script src={`https://hm.baidu.com/hm.js?${process.env.NEXT_PUBLIC_BAIDU_TONGJI_ID}`}/>
       </Html>
