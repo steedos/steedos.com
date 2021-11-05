e7fe0c7dea68fe8508e77d01ee84013cd76e468f
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-11-05 10:13:37 +0800

    始终在新窗口打开链接

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -108,7 +108,7 @@ export function a({ node, ...props }) {
       />
     }
 
-    const target = props.href.match(/https:\/\//i) || props.href.match(/http:\/\//i) ? "_blank": "_self";
+    const target = "_blank" //props.href.match(/https:\/\//i) || props.href.match(/http:\/\//i) ? "_blank": "_self";
     return <a href={props.href} target={target}>{props.children}</a>
   }
 }
