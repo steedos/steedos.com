5b73c653ca51fdcd9e3241e157bcfc083aff5aa8
廖大雪 <2291335922@qq.com>
2022-02-28 10:56:29 +0800

    视频分类排版（文档排版）https://github.com/steedos/steedos.com/issues/139

--- src/lib/video.js
+++ src/lib/video.js
@@ -3,37 +3,31 @@ import { fetchGraphql } from '@/lib/base'
 const QUERY_SITE_VIDEOS = `
 {
     _id,
-    body,
-    download_url,
-    duration,
-    hls_url,
-    is_free,
-    name,
-    owner,
-    published_at,
-    site,
-    summary,
     slug,
-    subtitles_url,
-    thumb_image,
-    owner__expand{
-        name
-    }
+    name,
+    sort_no,
+    description,
+    videos: _related_site_videos_collection(sort: "sort_no"){
+        _id,
+        slug,
+        name,
+        sort_no
+      }
+    
   }
 `
 
 export async function getVideos(){
     const query = `
         {
-            site_videos(sort: "sort_no")${QUERY_SITE_VIDEOS}
+            site_videos_collections(sort: "sort_no")${QUERY_SITE_VIDEOS}
         }
     `
     const result = await fetchGraphql(query);
-
     let videos = null;
 
-    if(result.data && result.data.site_videos){
-        videos = result.data.site_videos;
+    if(result.data && result.data.site_videos_collections && result.data.site_videos_collections.length > 0){
+        videos = result.data.site_videos_collections;
     }
     return videos;
 }
@@ -41,7 +35,26 @@ export async function getVideos(){
 export async function getVideo(slug){
     const query = `
         {
-            site_videos(sort: "sort_no", filters:["slug","=", "${slug}"])${QUERY_SITE_VIDEOS}
+            site_videos(sort: "sort_no", filters:["slug","=", "${slug}"])
+            {
+                _id,
+                body,
+                download_url,
+                duration,
+                hls_url,
+                is_free,
+                name,
+                owner,
+                published_at,
+                site,
+                summary,
+                slug,
+                subtitles_url,
+                thumb_image,
+                owner__expand{
+                    name
+                }
+            }
         }
     `
     const result = await fetchGraphql(query);
--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { getVideos,  } from '@/lib/video';
+import {NextSeo} from 'next-seo'
+import { Markdown } from '@/components/Markdown'
 
 export async function getStaticProps( {params,} ) {
   const videos = await getVideos();
@@ -16,60 +18,72 @@ export async function getStaticProps( {params,} ) {
 
 const SiteVideos: React.FC = (props: any) => {
   const { videos } = props;
+  const name = '视频中心';
+  const description = '欢迎使用华炎魔方低代码平台。';
   return (
-    <div className="text-black mx-auto max-w-screen-lg lg:py-16 py-10 px-4 sm:px-6 lg:px-8">
-      <h1 className="md:text-4xl text-2xl text-center font-bold pb-16">
-        视频
-      </h1>
-      <div className="grid md:grid-cols-2 grid-cols-1 md:gap-16 gap-8">
-        {videos.map((video: any) => {
-          return (
-            <div key={`/videos/${video.slug}`} className="flex flex-col">
-              {video.thumb_image ? (
-                <div className="md:mb-4 mb-2">
-                  <Link href={`/videos/${video.slug}`}>
-                    <a>
-                      <img
-                        src={`${process.env.NEXT_PUBLIC_STEEDOS_SERVER_ROOT_URL}/api/files/images/${video.thumb_image}`}
-                        alt={video.name}
-                        width={1280}
-                        height={720}
-                        className="rounded-lg"
-                      />
-                    </a>
-                  </Link>
-                </div>
-              ) : (
-                <div className="aspect-w-16 aspect-h-9 md:mb-4 mb-2">
-                  <Link href={`/videos/${video.slug}`}>
-                    <a>
-                      <div className="absolute top-0 left-0 w-full h-full bg-gray-200 dark:bg-gray-800 rounded-lg flex items-center justify-center text-gray-400 dark:text-gray-600">
-                        <IconPlaceholder />
-                      </div>
-                    </a>
-                  </Link>
-                </div>
-              )}
-              <Link href={`/videos/${video.slug}`}>
-                <a>
-                  <h2 className="md:text-2xl text-xl font-bold leading-tighter hover:text-blue-600 dark:hover:text-blue-300">
-                    {video.name}
-                  </h2>
-                </a>
-              </Link>
-              {/* <h2 className="uppercase font-semibold text-xs mt-2 text-gray-700 dark:text-gray-300">
-                {video.owner__expand.name}
-              </h2> */}
-              {video.summary && (
-                <div className="prose dark:prose-dark mt-2 text-gray-700 dark:text-white">
-                  {video.summary}
-                </div>
-              )}
-            </div>
-          )
-        })}
+    <>
+      <NextSeo
+        title={`${name}`}
+        description={description}
+      />
+    <div className="bg-white">
+      <div className=" max-w-screen-lg mx-auto pt-16 px-4 sm:pt-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+          {name}
+          </h2>
+          <p className="prose max-w-xl mt-5 mx-auto text-xl text-gray-500">
+          欢迎使用华炎魔方低代码平台，您可以免费访问这里的视频。我们为商业用户提供VIP支持服务，<a href="/collections/services" target="_blank">了解更多</a>。
+          </p>
+        </div>
       </div>
     </div>
+    <div className="text-black mx-auto max-w-screen-lg w-full lg:py-16 py-10 px-6">
+      <main>
+        <ul className="">
+        {videos && videos.map((videos_collection: any) => {
+          return (
+          <li className="py-12" key={videos_collection._id}>
+            <article className="">
+              <div className="mt-6 sm:mt-0">
+                <div className="space-y-4">
+                  <h2 className="text-3xl font-bold tracking-tight">
+                    {videos_collection.name}
+                  </h2>
+                  <div className="prose max-w-none text-gray-500">
+                    <div className="prose max-w-none">
+                      <Markdown body={videos_collection.description}></Markdown>
+                    </div>
+                  </div>
+                  <div className="text-base font-medium">
+                     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                      {videos_collection.videos && videos_collection.videos.map((video: any) => {                        
+                        const docSlug = `/videos/${video.slug}`
+                        return (
+                        <div
+                          key={video._id}
+                          className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                        >
+                          <div className="flex-1 min-w-0">
+                            <a href={docSlug} className="focus:outline-none">
+                              <span className="absolute inset-0" aria-hidden="true" />
+                              <p className="text-md font-medium text-gray-900">{video.name}</p>
+                            </a>
+                          </div>
+                        </div>
+                      )})}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </li>
+            )
+          })}
+        </ul>
+      </main>
+    </div>
+  </>
   )
 }
 
