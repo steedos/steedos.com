9dba1125fb0e95e0908762241f4eeb2dde521e0e
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2022-05-10 16:47:11 +0800

    文档左侧菜单支持滚动

--- src/layouts/DocumentLayout.tsx
+++ src/layouts/DocumentLayout.tsx
@@ -28,8 +28,8 @@ const DocumentLayout: FunctionComponent<LayoutProps> = ({
   <div className="hidden lg:block absolute top-0 bottom-0 right-0 left-1/2 bg-white"></div>
   <div className="relative flex w-full max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
     <div className="w-full flex-none lg:grid lg:grid-cols-3 lg:gap-8">
-      <div className="bg-gray-50 lg:bg-transparent -mx-4 sm:-mx-6 lg:mx-0 py-12 sm:py-16 px-4 sm:px-6 lg:pl-0 lg:pr-8">
-        <nav aria-label="Table of contents" className="text-sm max-w-[37.5rem] mx-auto lg:max-w-none lg:mx-0 relative lg:sticky lg:top-10" x-data="TableOfContents()" x-init="init()">
+      <div className="bg-gray-50 lg:bg-transparent -mx-4 sm:-mx-6 lg:mx-0 py-12 sm:py-6 px-4 sm:px-6 lg:pl-0 lg:pr-8">
+        <nav aria-label="Table of contents" className="text-sm max-w-[37.5rem] mx-auto lg:max-w-none lg:mx-0 lg:fixed w-full h-full pb-16 top-0 pt-32 overflow-y-auto" x-data="TableOfContents()" x-init="init()">
         <ul role="list" className="space-y-8 border-l border-gray-200 pl-6">
         {tableOfContents.map((section) => {
           let sectionIsActive =
