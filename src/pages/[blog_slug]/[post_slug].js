8a8a24fbecc01641926747b404a01ac21d11d8e8
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2022-05-25 15:13:18 +0800

    blog 菜单

--- src/pages/[blog_slug]/[post_slug].js
+++ src/pages/[blog_slug]/[post_slug].js
@@ -17,17 +17,225 @@ import tinytime from 'tinytime'
 import clsx from 'clsx'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
-import { PageHeader } from '@/components/PageHeader'
 import { getPost, getBlog, getPosts } from '@/lib/blog';
 import {NextSeo} from 'next-seo'
 // const {serialize} = require('next-mdx-remote/serialize')
 // import { MDXRemote } from 'next-mdx-remote'
 import { Heading } from '@/components/Heading';
 import { Markdown } from '@/components/Markdown'
+import {fromMarkdown} from 'mdast-util-from-markdown';
 
-const components = {
-  Heading,
+
+import { ContentsContext } from '@/layouts/ContentsLayout'
+
+import BananaSlug from 'github-slugger'
+import { Header } from '@/components/Header'
+
+const slugs = new BananaSlug()
+
+const getTOC = (markdown) => {
+
+  slugs.reset()
+  const tree = fromMarkdown(markdown)
+  
+  const contents = []
+
+  for (let i = 0; i < tree.children.length; i++) {
+    let node = tree.children[i]
+
+    if (node.type === 'heading' && [2, 3].includes(node.depth)) {
+      const level = node.depth
+      const title = node.children
+        .filter((n) => n.type === 'text')
+        .map((n) => n.value)
+        .join('')
+      let slug = slugs.slug(title, true) 
+
+      // let allOtherSlugs = contents.flatMap((entry) => [
+      //   entry.slug,
+      //   ...entry.children.map(({ slug }) => slug),
+      // ])
+      // let i = 1
+      // while (allOtherSlugs.indexOf(slug) > -1) {
+      //   slug = `${title}-${i}`
+      //   i++
+      // }
+
+      if (level === 2) {
+        contents.push({ title, slug, children: [] })
+      } else if (contents[contents.length - 1]){
+        contents[contents.length - 1].children.push({ title, slug })
+      }
+    }
+  }
+
+  return contents
 }
+
+function TableOfContents({ tableOfContents, currentSection }) {
+
+  function isActive(section) {
+    if (section.slug === currentSection) {
+      return true
+    }
+    if (!section.children) {
+      return false
+    }
+    return section.children.findIndex(isActive) > -1
+  }
+
+  let pageHasSubsections = tableOfContents.some((section) => section.children.length > 0)
+
+  return (
+    <>
+      <h5 className="text-slate-900 font-semibold mb-4 text-sm leading-6 dark:text-slate-100">
+        本页内容
+      </h5>
+      <ul className="text-slate-700 text-sm leading-6">
+        {tableOfContents.map((section) => (
+          <Fragment key={section.slug}>
+            <li>
+              <a
+                href={`#${section.slug}`}
+                className={clsx(
+                  'block py-1',
+                  pageHasSubsections ? 'font-medium' : '',
+                  isActive(section)
+                    ? 'font-medium text-sky-500 dark:text-sky-400'
+                    : 'hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300'
+                )}
+              >
+                {section.title}
+              </a>
+            </li>
+            {section.children.map((subsection) => (
+              <li className="ml-4" key={subsection.slug}>
+                <a
+                  href={`#${subsection.slug}`}
+                  className={clsx(
+                    'group flex items-start py-1',
+                    isActive(subsection)
+                      ? 'text-sky-500 dark:text-sky-400'
+                      : 'hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300'
+                  )}
+                >
+                  <svg
+                    width="3"
+                    height="24"
+                    viewBox="0 -9 3 24"
+                    className="mr-2 text-slate-400 overflow-visible group-hover:text-slate-600 dark:text-slate-600 dark:group-hover:text-slate-500"
+                  >
+                    <path
+                      d="M0 0L3 3L0 6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    />
+                  </svg>
+                  {subsection.title}
+                </a>
+              </li>
+            ))}
+          </Fragment>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+function useTableOfContents(tableOfContents) {
+  let [currentSection, setCurrentSection] = useState(tableOfContents[0]?.slug)
+  let [headings, setHeadings] = useState([])
+
+  const registerHeading = useCallback((id, top) => {
+    setHeadings((headings) => [...headings.filter((h) => id !== h.id), { id, top }])
+  }, [])
+
+  const unregisterHeading = useCallback((id) => {
+    setHeadings((headings) => headings.filter((h) => id !== h.id))
+  }, [])
+
+  useEffect(() => {
+    if (tableOfContents.length === 0 || headings.length === 0) return
+    function onScroll() {
+      let style = window.getComputedStyle(document.documentElement)
+      let scrollMt = parseFloat(style.getPropertyValue('--scroll-mt').match(/[\d.]+/)?.[0] ?? 0)
+      let fontSize = parseFloat(style.fontSize.match(/[\d.]+/)?.[0] ?? 16)
+      scrollMt = scrollMt * fontSize
+
+      let sortedHeadings = headings.concat([]).sort((a, b) => a.top - b.top)
+      let top = window.pageYOffset + scrollMt + 1
+      let current = sortedHeadings[0].id
+      for (let i = 0; i < sortedHeadings.length; i++) {
+        if (top >= sortedHeadings[i].top) {
+          current = sortedHeadings[i].id
+        }
+      }
+      setCurrentSection(current)
+    }
+    window.addEventListener('scroll', onScroll, {
+      capture: true,
+      passive: true,
+    })
+    onScroll()
+    return () => {
+      window.removeEventListener('scroll', onScroll, {
+        capture: true,
+        passive: true,
+      })
+    }
+  }, [headings, tableOfContents])
+
+  return { currentSection, registerHeading, unregisterHeading }
+}
+
+export function PageHeader({ title, description, repo, badge = {}, section }) {
+  if (!title && !description) return null
+
+  return (
+    <header id="header" className="relative z-20">
+      <div>
+        {section && (
+          <p className="mb-2 text-sm leading-6 font-semibold text-sky-500 dark:text-sky-400">
+            {section}
+          </p>
+        )}
+        <div className="flex items-center">
+          <h1 className="inline-block text-3xl sm:text-4xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200">
+            {title}
+          </h1>
+          {repo && (
+            <a
+              href={repo}
+              className="ml-3 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300 sm:mt-1 sm:ml-4"
+            >
+              <span className="sr-only">View on GitHub</span>
+              <svg
+                viewBox="0 0 16 16"
+                className="w-5 h-5 sm:w-6 sm:h-6"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+            </a>
+          )}
+        </div>
+        {badge.key && badge.value && (
+          <dl className="ml-3 mt-1.5 align-top inline-flex items-center px-3 py-1 rounded-full text-sm font-medium leading-4 bg-cyan-100 text-cyan-900 tracking-tight">
+            <dt className="sr-only">{badge.key}</dt>
+            <dd>{badge.value}</dd>
+          </dl>
+        )}
+      </div>
+      {description && (
+        <p className="mt-6 text-lg text-slate-700 dark:text-slate-400">{description}</p>
+      )}
+    </header>
+  )
+}
+
 
 export async function getStaticProps({
   params,
@@ -90,12 +298,16 @@ export default function Post(props) {
     return <Error statusCode={errorCode} />
   }
 
+  const toc = getTOC(body)
+  const { currentSection, registerHeading, unregisterHeading } = useTableOfContents(toc)
+
   let seo_title_calc = seo_title ? seo_title : name;
   if (blog && blog.name){
     seo_title_calc += ' - ' + blog.name
     // if (blog.site__expand) 
     //   seo_title_calc += ' | ' + blog.site__expand.name
   }
+  const section = blog.name
   const url = process.env.NEXT_PUBLIC_DEPLOYMENT_URL + router.asPath
   const imageUrl = image?process.env.NEXT_PUBLIC_STEEDOS_SERVER_ROOT_URL + `/api/files/images/${image}` : null
   return (
@@ -122,14 +334,7 @@ export default function Post(props) {
         // canonical={canonicalUrl}
       />
 
-      <div className="pt-10 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden bottom-10 bg-bottom bg-no-repeat bg-slate-50 dark:bg-[#0B1120] beams">
-        {/* <div
-            className="absolute inset-0 bg-grid-slate-900/[0.04] bg-[bottom_1px_center] dark:bg-grid-slate-400/[0.05] dark:bg-bottom dark:border-b dark:border-slate-100/5"
-            style={{
-              maskImage: 'linear-gradient(to bottom, transparent, black)',
-              WebkitMaskImage: 'linear-gradient(to bottom, transparent, black)',
-            }}
-        /> */}
+      {/* <div className="pt-10 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden bottom-10 bg-bottom bg-no-repeat bg-slate-50 dark:bg-[#0B1120] beams">
         <div className="mx-auto max-w-8xl lg:px-8">
           <div className="lg:grid lg:grid-cols-5 lg:gap-8">
             <div className="col-span-3 mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
@@ -144,7 +349,6 @@ export default function Post(props) {
             </div>
             <div className="col-span-2 mt-12 lg:m-0 lg:relative">
               <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
                 <img
                   className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                   src={imageUrl}
@@ -154,10 +358,11 @@ export default function Post(props) {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
       {/* <div className="w-full h-full bg-no-repeat absolute hidden lg:block" style={{backgroundImage: `url("${background_image}")`}}>
       </div> */}
-      <div className="w-full max-w-8xl mx-auto lg:flex px-4 sm:px-6 lg:px-8 z-10">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 md:px-8 z-10 pb-20">
+        <div className="lg:pl-[8rem]">
         {/* {blog.menu && blog.menu.items && (
         <nav id="sidebar" className="fixed z-40 inset-0 flex-none h-full lg:bg-gray-50 lg:bg-opacity-75 w-full lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden">
           <div id="navWrapper" className="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:bg-transparent overflow-hidden lg:top-18 bg-white mr-24 lg:mr-0">
@@ -186,14 +391,36 @@ export default function Post(props) {
           </div>
         </nav>
         )} */}
-        <div id="content-wrapper" className="lg:px-8 min-w-0 w-full flex-auto lg:static lg:max-h-full lg:overflow-visible">
-          <article className="pt-16 relative z-10 max-w-screen-lg mx-auto">
-            <main className="px-4 sm:px-6 md:px-8 py-8">
-              <Markdown body={body} className="prose sm:prose-lg lg:prose-xl"></Markdown>
-            </main>
-          </article>
+        <div className="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
+          <div id="content-wrapper" className="lg:px-8 min-w-0 w-full flex-auto lg:static lg:max-h-full lg:overflow-visible">
+            <article className="">
+            
+              <PageHeader
+                title={name}
+                description={summary}
+                section={section}
+              />
+              
+              <div
+                id="content-wrapper"
+                className="relative z-20 prose prose-slate mt-12 dark:prose-dark"
+              >
+                <ContentsContext.Provider value={{ registerHeading, unregisterHeading }}>
+                  <Markdown body={body} className="prose lg:prose-lg"></Markdown>
+                </ContentsContext.Provider>
+              </div>
+            </article>
+          </div>
+
+          <div className="fixed z-20 top-[3.8125rem] bottom-0 right-[max(0px,calc(50%-45rem))] w-[19.5rem] py-10 px-8 overflow-y-auto hidden xl:block">
+            {toc.length > 0 && (
+              <TableOfContents tableOfContents={toc} currentSection={currentSection} />
+            )}
+          </div>
         </div>
       </div>
+
+    </div>
     </>
   )
 }
