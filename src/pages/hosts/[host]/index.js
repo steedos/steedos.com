bc13f9f5ad9dcbf8f9883c820944bd9a5ebd3c91
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2022-05-28 22:11:55 +0800

    Update index.js

--- src/pages/hosts/[host]/index.js
+++ src/pages/hosts/[host]/index.js
@@ -59,11 +59,9 @@ function Header() {
             只需点击鼠标，就能快速创建应用程序，实现敏捷开发的新高度。
           </p>
           <div className="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
-            <NextLink href="/docs/">
-              <a className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400">
-                快速开始
-              </a>
-            </NextLink>
+            <a href="/docs/" className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400">
+              快速开始
+            </a>
             <SearchButton className="hidden sm:flex items-center w-72 text-left space-x-3 px-4 h-12 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500 shadow-sm rounded-lg text-slate-400 dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700">
               {({ actionKey }) => (
                 <>
