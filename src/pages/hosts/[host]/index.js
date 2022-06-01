154f56c6157c5e450b5b8c8c855d8d94fd9c4be3
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2022-06-01 18:23:22 +0800

    Update index.js

--- src/pages/hosts/[host]/index.js
+++ src/pages/hosts/[host]/index.js
@@ -21,6 +21,10 @@ import { Markdown } from '@/components/Markdown'
 import NextLink from 'next/link'
 import Head from 'next/head'
 import {getSite, getSiteByDomain, getSiteDomains} from '@/lib/site'
+import { getCollectionProducts, getCollections } from '@/lib/product';
+import ReviewStars from '@/components/product/ReviewStars'
+import Price from '@/components/product/Price'
+import { getDefaultPrice } from '@/lib/product.client';
 import { Player } from '@/components/player'
 import clsx from 'clsx'
 import styles from './index.module.css'
@@ -121,8 +125,12 @@ export async function getStaticProps({params}) {
   } = params
   const site = await getSiteByDomain(host)
   
+  const slug = 'steedos-packages'
+  const collection = await getCollectionProducts(slug)
+
   return {
     props: {
+      collection,
       ...site
     },
     notFound: !site || !site.homepage,
@@ -145,6 +153,7 @@ export async function getStaticPaths() {
 export default function Home(props) {
   const {
     name,
+    collection,
     homepage = {name: '', body: ''}
   } = props
   const [mobile, setMobile] = useState('')
@@ -211,6 +220,53 @@ export default function Home(props) {
         <ModernFeatures />
         <EditorTools />
         <ReadyMadeComponents /> */}
+
+
+
+            <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+              <div className="py-24 text-center">
+                
+                <h2 className="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white">
+                  开源开放，携手伙伴，打造海量解决方案
+                </h2>
+                <p className="mt-6 max-w-3xl mx-auto text-lg">
+                华炎魔方开放平台源码，携手全球合作伙伴，提供各行业各领域的低代码解决方案。基于华炎魔方开发的企业应用，无需开发人员介入，点击鼠标就能随心定制，快速满足企业数字化转型的各种需求。
+                </p>
+              </div>
+              <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+                {collection?.products?.map((product) => {
+
+                  const imageUrl = product.image?process.env.NEXT_PUBLIC_STEEDOS_SERVER_ROOT_URL + `/api/files/images/${product.image}` : null
+                  return (
+                  <div
+                    key={product.slug}
+                    className="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden"
+                  >
+                    <div className="aspect-w-5 aspect-h-3 bg-gray-200 group-hover:opacity-75">
+                      <img
+                        src={imageUrl}
+                        className="w-full h-full object-center object-cover sm:w-full sm:h-full"
+                      />
+                    </div>
+                    <div className="flex-1 p-4 space-y-2 flex flex-col">
+                      <h3 className="text-lg font-medium text-gray-900">
+                        <a href={`/products/${product.slug}`}>
+                          <span aria-hidden="true" className="absolute inset-0" />
+                          {product.name}
+                        </a>
+                      </h3>
+                      <p className="text-sm text-gray-500">{product.vender__expand?.name}</p>
+                      <p className="text-sm text-gray-500">{product.description}</p>
+                      <div className="flex-1 flex items-center justify-between">
+                        <p className="text-sm italic text-gray-500"><ReviewStars rating={product.rating}/></p>
+                        <p className="text-base font-medium text-gray-900"><Price price={getDefaultPrice(product)}></Price></p>
+                      </div>
+                    </div>
+                  </div>
+                  )
+                })}
+              </div>
+            </div>
       </div>
       {/* <Footer /> */}
     </>
