5e74a67c74cfe047eee8745f8d66adb2a463f98c
庄建国 <9340100@qq.com>
2022-08-17 07:41:27 +0000

    disable sentry

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const { withSyntaxHighlighting } = require('./remark/withSyntaxHighlighting')
 const { withNextLinks } = require('./remark/withNextLinks')
 const { withLinkRoles } = require('./rehype/withLinkRoles')
 const { withRemarkDirective } = require('./remark/withRemarkDirective')
+const { withSentryConfig } = require('@sentry/nextjs');
 const minimatch = require('minimatch')
 
 // const withExamples = require('./remark/withExamples')
@@ -60,7 +61,7 @@ const fallbackGetStaticProps = {
   'src/pages/blog/**/*': '@/layouts/BlogPostLayout',
 }
 
-module.exports = 
+const moduleExports = 
   withPlugins(
   [
   //   // withBundleAnalyzer({
@@ -333,3 +334,22 @@ module.exports =
     return config
   },
 });
+
+
+
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
+
+//   silent: true, // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
+
+// // Make sure adding Sentry options is the last code to run before exporting, to
+// // ensure that your source maps include changes from all other Webpack plugins
+// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = moduleExports
--- src/pages/_error.js.disabled
+++ src/pages/_error.js.disabled
@@ -22,7 +22,7 @@ import NextErrorComponent from 'next/error';
 const CustomErrorComponent = props => {
   // If you're using a Nextjs version prior to 12.2.1, uncomment this to
   // compensate for https://github.com/vercel/next.js/issues/8592
-  // Sentry.captureUnderscoreErrorException(props);
+  Sentry.captureUnderscoreErrorException(props);
 
   return <NextErrorComponent statusCode={props.statusCode} />;
 };
