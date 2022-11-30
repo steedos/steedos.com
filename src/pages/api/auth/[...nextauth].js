a268aa8cf2123ac09ea2c5a19a39751fd55f1bb0
庄建国 <9340100@qq.com>
2022-11-30 09:13:59 +0000

    OIDC SSO

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,105 +2,146 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-07-20 16:29:22
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-08-16 11:42:32
+ * @LastEditTime: 2022-11-19 16:02:57
  * @Description: 
  */
-import KeycloakProvider from "next-auth/providers/keycloak";
 
 import NextAuth from "next-auth"
+import KeycloakProvider from "@/lib/auth/KeycloakProvider";
+import CredentialsProvider from "@/lib/auth/CredentialsProvider";
 const axios = require('axios');
-const jwt = require("jsonwebtoken")
-const ROOT_URL = process.env.NEXT_PUBLIC_STEEDOS_ROOT_URL
-const JWT_API = '/accounts/jwt/login';
-const LOGOUT_API = '/accounts/logout';
-const STEEDOS_TOKENS = {};
+const querystring = require('querystring');
+const STEEDOS_ROOT_URL = process.env.NEXT_PUBLIC_STEEDOS_ROOT_URL;
+const OIDC_API = '/api/global/auth/oidc/login';
+const VALIDATE_API = '/api/setup/validate';
 
-const getJWTToken = (user)=>{
-  const jwtPayload = {
-    iss: process.env.NEXTAUTH_URL,
-    sub: "steedos-nextjs-amis",
-    profile: {
-      email: user.email,
-      ...user      
-    }
-  };
 
-  return jwt.sign(
-    jwtPayload,
-    process.env.STEEDOS_IDENTITY_JWT_SECRET,
-    {
-      expiresIn: 60
-    }
-  );
-}
-
-const loginSteedosProject = async (user)=>{
-  if(STEEDOS_TOKENS[user.email]){
-    return STEEDOS_TOKENS[user.email];
-  }
-  const projectRootUrl = ROOT_URL;
+const loginSteedosByOIDC = async (accessToken)=>{
+  const projectRootUrl = STEEDOS_ROOT_URL;
   const rest =  await axios({
-    url: `${projectRootUrl}${JWT_API}`,
-    method: 'get',
-    data: {},
-    headers: { "Content-Type": "application/json", "Authorization": `Bearer ${getJWTToken(user)}` }
+    url: `${projectRootUrl}${OIDC_API}`,
+    method: 'post',
+    data: {
+      accessToken,
+    },
+    headers: { "Content-Type": "application/json", "Authorization": `Bearer ${accessToken}` }
   });
-  STEEDOS_TOKENS[user.email] = rest.data;
-  return STEEDOS_TOKENS[user.email];
+  return rest.data
 }
 
-const logoutSteedosProject = (token)=>{
-  const projectRootUrl = process.env.NEXT_PUBLIC_STEEDOS_ROOT_URL;
-  axios({
-    url: `${projectRootUrl}${LOGOUT_API}`,
+const validateSteedosToken = async (space, token)=>{
+  const rest =  await axios({
+    url: `${STEEDOS_ROOT_URL}${VALIDATE_API}`,
     method: 'post',
-    data: {},
-    headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` }
-  }).catch((e)=>{
-    console.error(e);
-  })
+    data: {
+      utcOffset: 8
+    },
+    headers: { "Content-Type": "application/json", "Authorization": `Bearer ${space},${token}` }
+  });
+  return rest.data;
+}
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+ async function refreshAccessToken(token) {
+  try {
+    const url =
+      `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`
+
+    const response = await axios.post(url, 
+      querystring.stringify({
+        client_id: process.env.KEYCLOAK_ID,
+        client_secret: process.env.KEYCLOAK_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      })
+    )
+
+    const refreshedTokens = response.data
+    
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
 }
 
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
-  providers: [KeycloakProvider({
-    clientId: process.env.KEYCLOAK_ID,
-    clientSecret: process.env.KEYCLOAK_SECRET,
-    issuer: process.env.KEYCLOAK_ISSUER,
-    name: 'Steedos ID'
-  })],
+  providers: [
+    KeycloakProvider
+  ],
   callbacks: {
-  //   async jwt(props) {
-  //     const { token, account, user } = props;
-  //     // Persist the OAuth access_token to the token right after signin
-  //     // if (account) {
-  //     //   token.accessToken = account.access_token
-  //     // }
-  //     if(user && user.steedos){
-  //       token.steedos = user.steedos;
-  //     }
-  //     return token
-  //   }, 
-    async session({ session, token, user }) {
-      if(session.user){
-        if(token && token.steedos){
-          session.steedos = token.steedos;
-        }else{
-          try {
-            const loginResult = await loginSteedosProject(session.user);
-            if(loginResult.space && loginResult.token){
-              session.steedos = {
-                space: loginResult.space,
-                token: loginResult.token,
-                userId: loginResult.user?.id,
-                name: loginResult.user?.name
-              }
-            }
-          } catch (e) {console.error(e)}
+    async jwt(props) {
+      const { token, account, user } = props;
+
+      // Persist the OAuth access_token to the token right after signin
+      if (account && user) {
+        const token = {
+          accessToken: account.access_token,
+          accessTokenExpires: account.expires_at * 1000,
+          refreshToken: account.refresh_token,
+          provider: account.provider,
+          user,
         }
+
+        if (account.provider === 'keycloak') {
+          const loginResult = await loginSteedosByOIDC(account.access_token);
+          
+          user.space = loginResult.space;
+          user.token = loginResult.token;
+        }
+
+        if(user.space && user.token){
+          const steedosSession = await validateSteedosToken(user.space, user.token);
+          token.steedos = Object.assign(steedosSession, {token: steedosSession.authToken});
+        }
+
+        return token
       }
+
+      if (token.provider != 'keycloak') 
+        return token
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token
+      }
+
+      // Access token has expired, try to update it
+      return refreshAccessToken(token)
+    }, 
+
+    async session({ session, token, user }) {
+
+      session.user = token.user
+      session.steedos = token.steedos
+      session.accessToken = token.accessToken
+      session.error = token.error
+
+      session.publicEnv ={
+        STEEDOS_ROOT_URL: process.env.STEEDOS_ROOT_URL,
+        STEEDOS_EXPERIENCE_ASSETURLS: process.env.STEEDOS_EXPERIENCE_ASSETURLS,
+      }
+
       return session
+    }
+  },
+  events:{
+    async signOut(token, session){
     }
   },
   pages: {
