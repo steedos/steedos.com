23d16bbdb813f2575c9715c9fdcbd4dec9b2ffb7
庄建国 <9340100@qq.com>
2023-05-15 06:35:39 +0000

    headers

--- src/navs/header.js
+++ src/navs/header.js
@@ -33,7 +33,7 @@ export const headerNav = {
         },
         {
           id: 'features',
-          name: '功能',
+          name: '平台功能',
           href: '/videos',
           items: [
             { name: '数据建模', href: '/videos/lesson-object' },
@@ -118,69 +118,11 @@ export const headerNav = {
         },
       ],
     },
-    {
-      id: 'docs&videos',
-      name: '文档',
-      featured: [
-        {
-          name: '华炎魔方项目解决方案线上分享会',
-          href: '/videos/course-project',
-          imageSrc: 'https://console.steedos.cn/api/files/images/hqujpwHACBDxmMYyu',
-          imageAlt:
-            '华炎魔方项目解决方案线上分享会。',
-        },
-        {
-          name: '低代码训练营系列课程之业务建模',
-          href: '/videos/course-object',
-          imageSrc: 'https://console.steedos.cn/api/files/images/nob6cAXKMSiS8BBZP',
-          imageAlt:
-            '低代码训练营系列课程之业务建模',
-        },
-      ],
-      sections: [
-        {
-          id: 'docs',
-          name: '配置文档',
-          href: '/docs/',
-          items: [
-            { name: '安装部署', href: '/docs/deploy/getting-started' },
-            { name: '开发向导', href: '/docs/developer/getting-started' },
-            { name: '用户向导', href: '/docs/user/getting-started' },
-            { name: '管理员向导', href: '/docs/admin/getting-started' },
-            { name: '可视化建模', href: '/docs/admin/object' },
-            { name: '自动化流程', href: '/docs/admin/auto_process' },
-            { name: '页面设计', href: '/docs/amis/getting-started' },
-          ],
-        },
-        {
-          id: 'docs',
-          name: '开发文档',
-          href: '/docs/',
-          items: [
-            { name: '元数据介绍', href: '/docs/protocol/metadata-object' },
-            { name: '平台API', href: '/docs/developer/api-validate' },
-            { name: '平台微服务', href: '/docs/developer/service-objectql' },
-            { name: '微服务开发', href: '/docs/developer/service' },
-            { name: '集成插件', href: '/docs/plugins/node-red' },
-            { name: '元数据介绍', href: '/docs/protocol/metadata-object' },
-          ],
-        },
-        {
-          id: 'video',
-          name: '培训视频',
-          href: '/videos',
-          items: [
-            { name: '关于华炎魔方', href: '/videos' },
-            { name: '可视化开发教程', href: '/videos' },
-            { name: '低代码训练营课程回顾', href: '/videos' },
-            { name: '华炎魔方低代码DevOps平台', href: '/videos' },
-          ],
-        }
-      ],
-    },
+   
   ],
   pages: [
-    { name: '报价', href: '/pricing/platform' },
+    { name: '文档', href: '/docs/' },
     { name: '视频', href: '/videos/' },
+    { name: '联系我们', href: '/company/contact-us' },
   ],
 }
