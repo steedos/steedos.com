b54e0bccbb946449b261ddf8f58e22e3d1601cc6
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-02-22 09:19:50 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -517,8 +517,8 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
                       </a>
                     </>
                   )}
-                  <a className="hover:text-sky-500 dark:hover:text-sky-400 text-sm font-medium " href="https://demo.steedos.cn" target="_blank">
-                    <span className="ml-2 font-medium text-sm leading-5 rounded-full text-sky-600 bg-sky-400/10 px-3 py-2  dark:text-sky-400">在线试用</span>
+                  <a className="hover:text-sky-500 dark:hover:text-sky-400 text-sm font-medium " href="https://feikongwang.com/login/" target="_blank">
+                    <span className="ml-2 font-medium text-sm leading-5 rounded-full text-sky-600 bg-sky-400/10 px-3 py-2  dark:text-sky-400">试用解决方案</span>
                   </a>
                   </div>
                   {/* <div className="flex items-center border-l border-slate-200 ml-3 pl-6 dark:border-slate-800">
