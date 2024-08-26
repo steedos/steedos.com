ff2a2487fe9fdfc95998ac1635586f44761fde21
庄建国 <9340100@qq.com>
2024-08-26 10:00:49 +0000

    修正文档链接

--- src/components/salient/Hero.jsx
+++ src/components/salient/Hero.jsx
@@ -54,7 +54,7 @@ export function Hero() {
         <a target="_blank" href="/videos/" className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900">
           视频演示
         </a>
-        <a target="_blank" href="https://docs.steedos.cn/zh-CN/getting-started" className="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300">
+        <a target="_blank" href="https://docs.steedos.com/zh-CN/getting-started" className="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300">
           文档
         </a>
       </div>
