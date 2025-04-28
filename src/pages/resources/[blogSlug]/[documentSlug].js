afe059f1da7be73e9d3b7d2ed065438ca556bf94
Jack Zhuang <9340100@qq.com>
2025-04-28 19:31:20 +0800

    优化文章页面

--- src/pages/resources/[blogSlug]/[documentSlug].js
+++ src/pages/resources/[blogSlug]/[documentSlug].js
@@ -4,6 +4,7 @@ import {NextSeo} from 'next-seo'
 import { getProjectById, getBlogByUrl, getDocumentByUrl } from '@/b6/interfaces';
 import { RenderBuilderContent } from '@/b6/components/builder6';
 import { Markdown } from '@/components/Markdown'
+import moment from 'moment'
 
 export async function getStaticProps({params, query}) {
 
@@ -20,6 +21,7 @@ export async function getStaticProps({params, query}) {
       notFound: true,
     }
   }
+  blog.href = '/resources/' + blog.url;
 
   const document = await getDocumentByUrl(baseId, blog._id, params.documentSlug);
 
@@ -28,6 +30,7 @@ export async function getStaticProps({params, query}) {
       notFound: true,
     }
   }
+  console.log(document)
 
   return {
     props: {
@@ -57,10 +60,19 @@ export default function PageDetail({blog, document}){
         title={document.name}
         description={document.description}
         />
-        <div class="max-w-3xl mx-auto pt-16 pb-10 px-4">
-            <h1 class="text-2xl font-extrabold tracking-tight text-gray-900 md:text-4xl ">{document.name}</h1>
-            <div class="prose prose-lg py-10">
-              <Markdown body={document.markdown} className=""></Markdown>
+        <div class="mx-auto max-w-2xl lg:max-w-7xl px-6 lg:px-0">
+            {/* <a class="text-lg py-6 font-semibold text-sky-600" href={blog.href}>{blog.name}</a> */}
+            <h2 class="mt-16 font-mono text-xs/5 font-semibold tracking-widest text-gray-500 uppercase data-dark:text-gray-400">{moment(document.modified).fromNow()}</h2>
+            <h1 class="mt-2 text-4xl font-medium tracking-tighter text-pretty text-gray-950 data-dark:text-white sm:text-5xl sm:leading-[1.3]">{document.name}</h1>
+            <div class="mt-16 grid grid-cols-1 gap-8 pb-24 lg:grid-cols-[15rem_1fr] xl:grid-cols-[15rem_1fr_15rem]">
+              <div class="flex flex-wrap items-center gap-8 max-lg:justify-between lg:flex-col lg:items-start">
+                <div class="flex flex-wrap gap-2">
+                  <a class="rounded-full border border-dotted border-gray-300 bg-gray-50 px-2 text-sm/6 font-medium text-gray-500" data-headlessui-state="" href={blog.href}>{blog.name}</a>
+                </div>
+              </div>
+              <div class="prose prose-xl max-w-3xl xl:mx-auto">
+                <Markdown body={document.markdown} className=""></Markdown>
+              </div>
             </div>
         </div>
       </>
