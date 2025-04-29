feae44121183767954350d58c2f2d7687cf224e4
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-04-29 15:41:31 +0800

    Update [documentSlug].js

--- src/pages/resources/[blogSlug]/[documentSlug].js
+++ src/pages/resources/[blogSlug]/[documentSlug].js
@@ -61,7 +61,6 @@ export const getStaticPaths = (async () => {
 
 export default function PageDetail({blog, document}){
 
-  console.log('post', document)
   if (document && document.markdown) {
     return (
       <>
@@ -72,13 +71,13 @@ export default function PageDetail({blog, document}){
         <div class="mx-auto max-w-2xl lg:max-w-7xl px-6 lg:px-8 py-16 lg:py-24">
             <div class="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"><span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span><span class="ml-3">{moment(document.modified).format('LL')}</span></div>
             <h1 class="mt-2 text-4xl font-bold tracking-tighter text-pretty text-gray-950 data-dark:text-white sm:text-5xl sm:leading-[1.3]">{document.name}</h1>
-            <div class="mt-16 grid grid-cols-1 gap-8 pb-24 lg:grid-cols-[15rem_1fr] xl:grid-cols-[15rem_1fr_15rem]">
+            <div class="mt-16 grid grid-cols-1 gap-8 pb-24 lg:grid-cols-[12rem_1fr] xl:grid-cols-[12rem_1fr_12rem]">
               <div class="flex flex-wrap items-center gap-8 max-lg:justify-between lg:flex-col lg:items-start">
                 <div class="flex flex-wrap gap-2">
                   <a class="rounded-full border border-dotted border-gray-300 bg-gray-50 px-2 text-sm/6 font-medium text-gray-500" data-headlessui-state="" href={blog.href}>{blog.name}</a>
                 </div>
               </div>
-              <div class="prose prose-lg max-w-3xl xl:mx-auto">
+              <div class="prose prose-lg max-w-4xl xl:mx-auto">
                 {/* {document.cover && (
                   <img src={`https://builder6.steedos.cn/api/files/images/${document.cover}`} class="mb-10 aspect-3/2 w-full rounded-2xl object-cover"/>
                 )} */}
