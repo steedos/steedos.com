fa304ba790decf5392e30a8e96977942b81f0d82
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-05-15 09:45:02 +0800

    fix theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -488,11 +488,11 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
                     <span className="ml-2 font-medium text-sm leading-5 rounded-full text-gray-600 px-3 py-2  dark:text-gray-400">联系我们</span>
                   </a>
                     <div class="dropdown dropdown-hover">
-                      <div tabindex="0" role="button" class="ml-2 font-medium text-sm leading-5 rounded-full text-sky-600 bg-sky-400/10 px-3 py-2  dark:text-sky-400">在线试用</div>
-                      <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                      <div tabindex="0" role="button" class="ml-2 font-medium text-sm leading-5 rounded-full text-sky-600 bg-sky-400/10 px-3 py-2  dark:text-sky-400"><a href="https://demo.steedos.cn/" target="_blank">在线试用</a></div>
+                      {/* <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                         <li><a href="https://demo.steedos.cn/" target="_blank">低代码平台</a></li>
                         <li><a href="https://feikongwang.steedos.cn/" target="_blank">费控解决方案</a></li>
-                      </ul>
+                      </ul> */}
                     </div>
                   </div>
                   
